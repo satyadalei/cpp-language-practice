a307999edf8a8ccf0fe3aad101a0783f0fb90cb6
Satyanarayan <satyanarayandalei65@gmail.com>
2023-04-01 09:29:01 +0530

    Practicing - getting line of texts

--- reading_a_line_of_text.cpp
+++ reading_a_line_of_text.cpp
@@ -6,12 +6,16 @@ int main()
     /*Reading a line of text*/
     char name[30];
     cout<<"Enter name :";
+
+    //getline() method --> to take text until enter button is placed with or withoud space
     cin.getline(name,30); // reads text upto 30 characters including spaces.
     cout<<"\n Enter a paragraph :";
     char paragraph[100];
+    // get() method --> to take text of paragraph
     cin.get(paragraph,100);
 
     cout<<name<<endl;
     cout<<paragraph<<endl;
+    //cout<<paragraph[2];
     return 0;
 }
